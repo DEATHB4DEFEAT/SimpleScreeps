7cdeda3e00ed6ba9c0a645f40fd8e3c0839b71ea
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-11-02 14:37:53 -0700

    update nix

--- shell.nix
+++ shell.nix
@@ -1,30 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  lib = import <nixpkgs/lib>;
-  buildNodeJs = pkgs.callPackage "${<nixpkgs>}/pkgs/development/web/nodejs/nodejs.nix" {
-    python = pkgs.python3;
-  };
-
-  nodejsVersion = lib.fileContents ./.nvmrc;
-
-  # nodejs = buildNodeJs {
-  #   enableNpm = false;
-  #   version = nodejsVersion;
-  #   sha256 = "";
-  # };
-
-  NPM_CONFIG_PREFIX = toString ./npm_config_prefix;
-
+  	NPM_CONFIG_PREFIX = toString ./npm_config_prefix;
 in pkgs.mkShell {
-  packages = with pkgs; [
-    nodejs
-    nodePackages.npm
-  ];
+	packages = with pkgs; [
+		nodejs
+		nodePackages.npm
+	];
 
-  inherit NPM_CONFIG_PREFIX;
+	inherit NPM_CONFIG_PREFIX;
 
-  shellHook = ''
-    export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
-  '';
+	shellHook = ''
+		export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+	'';
 }
