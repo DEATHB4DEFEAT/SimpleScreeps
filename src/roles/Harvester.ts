09a05da074fc4803f8c4008d2b32ca19ddab8393
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-11-03 16:33:56 -0800

    harvester trasfers to storage

--- src/roles/Harvester.ts
+++ src/roles/Harvester.ts
@@ -1,16 +1,59 @@
 import Role from '../utils/Role';
+import Tasks from '../utils/Tasks';
+
 
 export default class Harvester extends Role {
 	role: string = 'Harvester';
-	traits: BodyPartConstant[] = [WORK, CARRY, MOVE];
-	requestedCreeps: number = 3;
+	traits: BodyPartConstant[] = [ WORK, CARRY, MOVE, MOVE ];
+	requestedCreeps: number = 4;
 	loop: Function = (creep: Creep) => {
 		new Role().loop(creep);
 
-		if (creep.store.getFreeCapacity() > 0) {
+
+		if (!creep.memory.task) {
+			if (creep.store.getFreeCapacity() > 0)
+				creep.memory.task = Tasks.HARVEST;
+			else {
+				if (creep.store[RESOURCE_ENERGY] > 0)
+					creep.memory.task = Tasks.DEPOSIT_ENERGY;
+			}
+		}
+
+
+		if (creep.memory.task == Tasks.DEPOSIT_ENERGY && creep.store[RESOURCE_ENERGY] > 0) {
+			if (creep.memory.target && (Game.getObjectById(creep.memory.target) as Structure).structureType == STRUCTURE_CONTAINER)
+				delete creep.memory.target;
+
+			let structure = creep.memory.target ? Game.getObjectById(creep.memory.target) as StructureContainer : undefined;
+			if (!structure) {
+				structure = creep.room.find(FIND_STRUCTURES).find(s => s.structureType == STRUCTURE_CONTAINER) as StructureContainer;
+				if (!structure) {
+					creep.memory.task = Tasks.SUPPLY_SPAWN;
+					return;
+				}
+				creep.memory.target = structure.id;
+			}
+
+			const code = creep.transfer(structure, RESOURCE_ENERGY);
+			if (code == ERR_NOT_IN_RANGE)
+				creep.moveTo(structure);
+			if (code == ERR_FULL)
+				creep.memory.task == Tasks.SUPPLY_SPAWN;
+			if (code == ERR_INVALID_TARGET)
+				delete creep.memory.target;
+		}
+
+		else if (creep.memory.task == Tasks.SUPPLY_SPAWN && creep.store[RESOURCE_ENERGY] > 0) {
+			delete creep.memory.target;
+
+			if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+				creep.moveTo(Game.spawns['Spawn1']);
+		}
+
+		else if (creep.memory.task == Tasks.HARVEST && creep.store.getFreeCapacity() > 0) {
 			const source = creep.memory.target ?? creep.room.find(FIND_SOURCES)[0].id;
 			creep.memory.target = source;
-			const sourceBlock = Game.getObjectById(source)!;
+			const sourceBlock = Game.getObjectById(source);
 
 			const code = creep.harvest(sourceBlock)
 			if (code == ERR_NOT_IN_RANGE)
@@ -18,11 +61,10 @@ export default class Harvester extends Role {
 			if (code == ERR_INVALID_TARGET)
 				delete creep.memory.target;
 		}
+
 		else {
 			delete creep.memory.target;
-
-			if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-				creep.moveTo(Game.spawns['Spawn1']);
+			delete creep.memory.task;
 		}
 	}
 }
